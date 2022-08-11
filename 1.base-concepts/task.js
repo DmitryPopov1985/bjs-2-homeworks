c03a27401f5b31732d71bea214b35fef7051e2c5
Дмитрий Попов <mitjajwars@mail.ru>
2022-08-11 17:14:58 +0300

    Доработка задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -18,30 +18,25 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   
   let totalAmount;
   
-
-
-
-
-
-  let creditBody = amount - contribution;
-  
-  let monthlyPercent = percent / 12 / 100;
-  const n = Math.ceil((date - new Date()) / 1000 / 60 / 60 / 24 /30.5);
-  
-  let monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**n) - 1)));
-  
-  console.log(creditBody);
-  console.log(monthlyPayment);
   
   if (isNaN(percent)) {
-    totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
   } else if (isNaN(contribution)) {
-    totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
   } else if (isNaN(amount)) {
-    totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
-  } else {
-    totalAmount = +(Number(contribution) + (monthlyPayment * n)).toFixed(2);
-  }
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  } 
+  
+
+
+
+  
+  let creditBody = amount - contribution;
+  let monthlyPercent = percent / 12 / 100;
+  const n = Math.ceil((date - new Date()) / 1000 / 60 / 60 / 24 /30.5);
+  let monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**n) - 1)));
+  totalAmount = +(monthlyPayment * n).toFixed(2);
+  
   
   return totalAmount;
   
