3146f799d2596d8df01b52a719eab4fe641ea513
Дмитрий Попов <mitjajwars@mail.ru>
2022-08-11 05:03:49 +0300

    Основные понятия.Доработка

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,19 +3,11 @@
 function solveEquation(a, b, c) {
   let arr = [];
   // код для задачи №1 писать здесь
-<<<<<<< HEAD
   let d = b**2-4*a*c;
 
-  if (d == 0) {
-=======
-  let d;
-  d = b**2-4*a*c;
 
-  if (d < 0) {
-    arr = [];
-  } else if (d == 0) {
->>>>>>> d2309092c682db50ca7196d9cdaf9f4c4e159137
-    arr = [-b/(2*a)];
+  if (d == 0) {
+   arr = [-b/(2*a)];
   } else if (d > 0) {
     arr = [(-b + Math.sqrt(d) )/(2*a) , (-b - Math.sqrt(d) )/(2*a)];
   }
@@ -26,18 +18,11 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   
   let totalAmount;
   
-<<<<<<< HEAD
 
 
 
-=======
-  
-  isNaN(percent) ? alert('`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`') : Number(percent);
-  isNaN(contribution) ? alert('`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`') : Number(contribution);
-  isNaN(amount) ? alert('`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`') : Number(amount);
 
 
->>>>>>> d2309092c682db50ca7196d9cdaf9f4c4e159137
   let creditBody = amount - contribution;
   
   let monthlyPercent = percent / 12 / 100;
@@ -45,9 +30,8 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   
   let monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**n) - 1)));
   
-<<<<<<< HEAD
-  
-
+  console.log(creditBody);
+  console.log(monthlyPayment);
   
   if (isNaN(percent)) {
     totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
@@ -56,11 +40,9 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   } else if (isNaN(amount)) {
     totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
   } else {
-    totalAmount = (Number(contribution) + (monthlyPayment * n)).toFixed(2);
+    totalAmount = +(Number(contribution) + (monthlyPayment * n)).toFixed(2);
   }
-=======
-  totalAmount = (Number(contribution) + (monthlyPayment * n)).toFixed(2);
->>>>>>> d2309092c682db50ca7196d9cdaf9f4c4e159137
+  
   return totalAmount;
   
 }
