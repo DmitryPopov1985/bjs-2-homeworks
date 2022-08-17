00e7a6b0f15366a17e0d11eb6f31ad94adbe8c98
Дмитрий Попов <mitjajwars@mail.ru>
2022-08-17 12:02:44 +0300

    Доработка

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,11 @@
 function compareArrays(arr1, arr2) {
   
-  
-  
-
   return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
  
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter(item => item >= 0).filter(item => item % 3 === 0).map(item => item * 10);
   
-  return resultArr;
+  return arr.filter(item => item >= 0).filter(item => item % 3 === 0).map(item => item * 10);
+  
 }
