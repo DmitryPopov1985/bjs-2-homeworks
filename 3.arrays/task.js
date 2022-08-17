576ad1753ad353bc36b1702e411111af2244f44f
Дмитрий Попов <mitjajwars@mail.ru>
2022-08-17 12:00:45 +0300

    Доработка

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result;
   
-  if(arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])) {
-    result = true;
-  } else {
-    result = false;
-  }
+  
   
 
-  return result;
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
+ 
 }
 
 function advancedFilter(arr) {
